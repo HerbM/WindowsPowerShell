ffab4aee5f42d093b1df08672eeb1873cdc72e24
Jesse M Ramirez Sr <jramirez@effcomsys.com>
2018-02-07 22:23:57 -0600

    Git notes from ECS

---
+++ work/NotesFromGitProfile.ps1
@@ -0,0 +1,38 @@
+
+new-alias np S:\Programs\Portable\Notepad++Portable\Notepad++Portable.exe -force -scope global
+
+<#
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force                                                          
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass                                                        
+.\Import-SNPTimeSheet.ps1                                                                                         
+.\Import-SNPTimeSheetV3-5.ps1                                                                                     
+
+  while ($true) {
+    
+  }
+  
+get-item .\Import-SNPTimeSheetV3-5.ps1 -stream *                                                                  
+Set-ExecutionPolicy -Scope LocalMachine  -ExecutionPolicy RemoteSigned                                            
+Set-ExecutionPolicy -Scope LocalMachine  -ExecutionPolicy RemoteSigned                                            
+Set-ExecutionPolicy -Scope LocalMachine  -ExecutionPolicy RemoteSigned -force                                     
+get-ExecutionPolicy   -scope CurrentUser                                                                          
+get-ExecutionPolicy   -scope Process                                                                              
+$env:psmodulepath -split ';'                                                                                      
+md WindowsPowerShell                                                                                              
+cd .\WindowsPowerShell\                                                                                           
+where.exe git                                                                                                     
+git --version                                                                                                     
+dir -force     
+
+####
+cd (Split-Path $Profile)
+git init                                                                                                                                                                                        
+git remote add HerbProfile https://github.com/HerbM/Profile-Utilities                                             
+git remote -v                                                                                                     
+git fetch --all                                                                                                   
+git reset --hard HerbProfile/master                                                                               
+git pull HerbProfile master                                                                                       
+####
+cd S:\Programs\Portable\                                                                                          
+
+#>
---
+++ work/get-apptime.ps1
@@ -0,0 +1,13 @@
+function get-apptime ($x) {
+
+
+
+
+
+
+
+<#
+
+
+
+#>
