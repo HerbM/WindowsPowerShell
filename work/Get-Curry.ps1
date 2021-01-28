1447d082fb62ac2298dedf080bd7f84de3b81302
HerbM <HerbMartin@GMail.com>
2021-01-28 15:32:50 -0600

    Updated Everything 1.4.1.1005 & ES 1.1.0.18

---
+++ work/Get-Curry.ps1
@@ -0,0 +1,24 @@
+<#
+.Synopsis 
+Curry a PowerShell function or cmdlet
+.Description
+Return a curried PowerShell function supplied with default paraters provided by caller
+.Notes
+Build a function or scriptblock closure with PSDefaultParameterValues closed inside
+
+#>
+[Get-CmdletBinding()]Param(
+  [Parameter(Mandatory)]$Name,
+  [Parameter(Mandatory)]$NewName,
+  [Hashtable]$Parameters = @{},       
+  [Switch]$Test                  # Dummy for development simplicity   
+)
+
+
+New-Module -Name $Name$NewName {
+  $PSDefaultParameterValues = Get-Variable PSDefaultParameterValues -Scope 1
+  ForEach ($Key in $Parameters.Keys) {
+     $PSDefaultParameterValues."$($Name):$Key" = $Parameters.$Key  ## Need to convert to Name:Parameter
+  }
+
+}
