81095b0f2f3367144b8970d8edbbf1e84e588c50
Herb Martin <HerbMartin@Gmail.com>
2021-05-16 21:58:54 -0500

    RipCheck 0.0.19 Adds ping capability

--- Scripts/Add-AliasList.ps1
+++ Scripts/Add-AliasList.ps1
@@ -1,3 +1,4 @@
 New-Alias winmerge 'C:\Program Files\WinMerge\WinMergeU.exe' -Force -Scope Global
 New-Alias dumpbin 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Tools\MSVC\14.28.29617\bin\Hostx64\x86\dumpbin.exe' -force -scope Global
 New-Alias gh Get-Help -scope Global -force
+New-Alias dumpbin 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Tools\MSVC\14.28.29617\bin\Hostx64\x86\dumpbin.exe' -force -scope Global
